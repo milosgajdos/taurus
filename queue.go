07901e098035fa8d48e14958721f19df58b92803
Milos Gajdos <milosgajdos83@gmail.com>
2015-07-22 16:55:01 +0100

    Adding some queue docs.

--- queue.go
+++ queue.go
@@ -6,16 +6,27 @@ const (
 	PendingQ = "pending"
 )
 
+// Subscription is a TaskQueue topic subscription
 type Subscription interface {
+	// ReadTask waits for some time to read Task from TaskQueue
 	ReadTask(time.Duration) (*Task, error)
+	// AutoUnsubscribe automatically unsubscribes from Subscription once some
+	// number of messages have been received
 	AutoUnsubscribe(int) error
+	// Unsubscribe unsubscribes from Subscription
 	Unsubscribe() error
+	// TimedOut checks if the error returned from Subscription read means TaskQueue timed out
 	TimedOut(error) bool
+	// Closed checks if the error returned from Subscription read means TaskQueue is Closed
 	Closed(error) bool
 }
 
+// TaskQueue is a generic Task queu
 type TaskQueue interface {
+	// Publish publishes data to a TaskQueue
 	Publish(string, interface{}) error
+	// Subscribe subscribes to a given topic and returns Subscription
 	Subscribe(string) (Subscription, error)
+	// Close closes the TaskQeue
 	Close()
 }
--- queue/basicqueue.go
+++ queue/basicqueue.go
@@ -15,11 +15,21 @@ const (
 
 var DefaultOptions = &nats.DefaultOptions
 
+// BasicQueue provides a basic implementation of taurus.TaskQueue interface
+// BasicQueue uses nats.io distributed queue for data transport
 type BasicQueue struct {
 	enconn *nats.EncodedConn
 }
 
+// NewBasicQueue initializes BasicQueue
+//
+// NewBasicQueue accepts nats.Options and encoding type which must be supported by nats.io queue
+// If no options are provided, BasicQueue will be initialized with DefaultOptions
+// It returns error if the queue fails to be initialized.
 func NewBasicQueue(options *nats.Options, encType string) (*BasicQueue, error) {
+	if options == nil {
+		options = DefaultOptions
+	}
 	conn, err := options.Connect()
 	if err != nil {
 		return nil, err
@@ -35,10 +45,14 @@ func NewBasicQueue(options *nats.Options, encType string) (*BasicQueue, error) {
 	}, nil
 }
 
+// Publish publishes data to a given queue for a given subject topic
+// It returns an error if publishing the data to queue failed.
 func (bq *BasicQueue) Publish(subject string, data interface{}) error {
 	return bq.enconn.Publish(subject, data)
 }
 
+// Subscribe creates a Synchronous subscription to given subject.
+// It returns error if the subscribing fails. This can be due to closed queue.
 func (bq *BasicQueue) Subscribe(subject string) (taurus.Subscription, error) {
 	sub, err := bq.enconn.Conn.SubscribeSync(subject)
 	if err != nil {
@@ -49,18 +63,27 @@ func (bq *BasicQueue) Subscribe(subject string) (taurus.Subscription, error) {
 	}, nil
 }
 
+// Close closes the Queue connection
+// All subsequent Subscribe and Publish calls will fail from now on with nats.ErrConnectionClosed error
 func (bq *BasicQueue) Close() {
 	bq.enconn.Close()
 }
 
+// Conn returns a raw nats queue connection
 func (bq *BasicQueue) Conn() *nats.Conn {
 	return bq.enconn.Conn
 }
 
+// TaskSubscription provides a generic implementation of taurus.Subscription interface
+// On low level it wraps *nats.Subscription and provides a simple API to it
 type TaskSubscription struct {
 	sub *nats.Subscription
 }
 
+// ReadTask waits for data to arrive on a given Subscription topic for a provided timeout time
+//
+// ReadTask umarshals the raw data based on encodeing type into taurus.Task and returns it
+// It returns error if either unmarshaling data failed, queue has been closed or a time out occurred
 func (ts *TaskSubscription) ReadTask(timeout time.Duration) (*taurus.Task, error) {
 	m, err := ts.sub.NextMsg(timeout)
 	if err != nil {
@@ -73,14 +96,18 @@ func (ts *TaskSubscription) ReadTask(timeout time.Duration) (*taurus.Task, error
 	return task, nil
 }
 
+// AutoUnsubscribe automatically unsubscribes client from Subscription once max messages have been received
 func (ts *TaskSubscription) AutoUnsubscribe(max int) error {
 	return ts.sub.AutoUnsubscribe(max)
 }
 
+// Unsubscribe unsubscribes client from subscription
 func (ts *TaskSubscription) Unsubscribe() error {
 	return ts.sub.Unsubscribe()
 }
 
+// TimedOut inspects the error passed in as a parameter and returns true or false based on the type of error
+// If the passed in error is nats.ErrTimeout it returns true. Otherwise it returns false
 func (ts *TaskSubscription) TimedOut(err error) bool {
 	if err == nats.ErrTimeout {
 		return true
@@ -88,6 +115,8 @@ func (ts *TaskSubscription) TimedOut(err error) bool {
 	return false
 }
 
+// Closed inspects the error passed in as a parameter and returns true or false based on the type of error
+// If the passed in error is nats.ErrConnectionClosed it returns true. Otherwise it returns false.
 func (ts *TaskSubscription) Closed(err error) bool {
 	if err == nats.ErrConnectionClosed {
 		return true
